46d6d4aaf4fd23ba92c76d229e4ebf15a4965dd5
Nicolas Kruchten <nicolas@datacratic.com>
2016-01-02 17:25:30 -0500

    bundling js and css and reducing calls to third parties

---
+++ js/bundle.js
@@ -0,0 +1,8 @@
+---
+layout: merge
+to_merge:
+    - jquery-1.9.1.min.js
+    - bootstrap.min.js
+    - featherlight.min.js
+    - gh-pages-blog.js
+---
---
+++ js/gh-pages-blog.js
@@ -0,0 +1,39 @@
+
+    $(function(){
+        /* main page */
+        $('.span7 > .ghpb-post > img')
+            .add('.span7 > .ghpb-post > p > img')
+            .css("max-height", "400px")
+            .each(function(){
+                $(this).featherlight($(this).attr("src"));
+                $(this)
+                    .css({"padding-bottom": "20px", "cursor": "pointer"})
+                    .wrap('<div class="row">')
+                    .wrap('<div class="span5 offset1 text-center">')
+                    ;
+            });
+        
+        /* in post */
+        $('.span8 > .ghpb-post > p > img')
+            .css("max-height", "400px")
+            .each(function(){
+                $(this).featherlight($(this).attr("src"));
+                $(this)
+                    .css({"padding-bottom": "20px", "cursor": "pointer"})
+                    .parent()
+                    .wrap('<div class="row postimg">')
+                    .wrap('<div class="span6 offset1 text-center">')
+                    ;
+            });
+        
+        /* in post, in link */
+        $('.span8 > .ghpb-post > p > a > img')
+            .css("max-height", "400px")
+            .each(function(){
+                $(this)
+                    .parent().parent()
+                    .wrap('<div class="row postimg">')
+                    .wrap('<div class="span6 offset1 text-center">')
+                    ;
+            });
+    });
